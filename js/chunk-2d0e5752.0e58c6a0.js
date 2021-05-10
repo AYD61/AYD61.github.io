5cd3ae9e4b9edae38f41fe9759fcf699ec7733ba
AYD <51245362+AYD61@users.noreply.github.com>
2021-05-10 18:43:58 +0800

    Revert "0510"
    This reverts commit dbc73b5379853178c737c0c32aa2e3d52d4d35d6.

--- js/chunk-2d0e5752.0e58c6a0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5752"],{9522:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h2",[n._v("购物车")])},u=[],a={name:"cart"},l=a,r=t("2877"),s=Object(r["a"])(l,c,u,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0e5752.0e58c6a0.js.map
--- js/chunk-2d230fd9.9e4de341.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fd9"],{ef46:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h2",[n._v("关于我")])},a=[],c={name:"about"},l=c,o=t("2877"),r=Object(o["a"])(l,u,a,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d230fd9.9e4de341.js.map
